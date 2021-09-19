d14fe82faac7ae8401298dc20a564769d70c81a5
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-09-20 03:34:13 +0700

    Add Solana tools

--- metaplex/flake.nix
+++ metaplex/flake.nix
@@ -9,12 +9,13 @@
 
       # Helpers to instantiate nixpkgs for supported system types.
       give = with nixpkgs.lib; let forAllSystems = f: genAttrs givenSystems (system: f system); in
-      f: forAllSystems (system: f (import nixpkgs { inherit system; overlays = [ self.overlay solnix.overlay ]; }));
+      f: forAllSystems (system: f (import nixpkgs { inherit system; overlays = [ solnix.overlay self.overlay ]; }));
     in
     {
       # nix develop
       devShell = give ({ pkgs, mkShell, lib, ... }: with lib;
         mkShell {
+          packages = with pkgs; [ nodejs yarn cargo solana ];
           shellHook = with pkgs.scripts;
             optionalString (!(pathExists ./.gitignore)) ''
               ${cloneFromGithub {
@@ -30,6 +31,7 @@
       # override pkgs
       overlay = final: prev: with prev.lib; {
         nodejs = prev.nodejs-16_x;
+        solana = prev.solana-testnet;
       };
     };
 }
--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,8 @@
-final: prev: with prev.lib; {
+final: prev: with prev.lib; let
+  target = with systems.parse; tripleFromSystem (mkSystemFromString final.system);
+  inherit (final.stdenv) mkDerivation;
+in
+{
   scripts = with final; rec {
     copy-derivation =
       let copy = writeScript "cp" ''
@@ -13,5 +17,26 @@ final: prev: with prev.lib; {
       writeScript "clone" ''
         ${copy-derivation} ${src} $1
       '';
+  };
+
+  solana-testnet = mkDerivation rec {
+    pname = "solana";
+    version = "1.7.12";
+    src = fetchTarball {
+      url = "https://github.com/solana-labs/solana/releases/download/v${version}/${pname}-release-${target}.tar.bz2";
+    };
+    installPhase = ''
+      install -Dm755 bin/* -t $out/bin
+    '';
+  };
+  solana = mkDerivation rec {
+    pname = "solana";
+    version = "1.6.25";
+    src = fetchTarball {
+      url = "https://github.com/solana-labs/solana/releases/download/v${version}/${pname}-release-${target}.tar.bz2";
+    };
+    installPhase = ''
+      install -Dm755 bin/* -t $out/bin
+    '';
   };
 }
